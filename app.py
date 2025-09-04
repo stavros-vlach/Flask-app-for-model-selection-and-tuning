cf64096d6ce131d92662433603ee594b53d70eba
Stavros Vlachoutsikos <122750141+stavros-vlach@users.noreply.github.com>
2025-09-04 14:00:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import pandas as pd
 from model import train_model
+from utils import preprocess_data
+from sklearn.datasets import load_iris
 
 app = Flask(__name__)
 
@@ -11,13 +13,18 @@ def index():
         model_name = request.form.get("model_name")
 
         file = request.files.get("dataset")
-        if file:
-            df = pd.read_csv(file, sep=None, engine='python')
+        if file and file.filename != "":
+            df = pd.read_csv(file, sep=None, engine="python")
+        else:
+            iris = load_iris(as_frame=True)
+            df = iris.frame
+            df["target"] = iris.target 
 
-            X = df.iloc[:, :-1].values
-            y = df.iloc[:, -1].values
+        X, y, messages = preprocess_data(df)
 
-            results = train_model(model_name, X, y)
+        results = train_model(model_name, X, y)
+        results["messages"] = messages
+            
         return render_template("result.html", results=results)
 
     return render_template("index.html", results=results)
