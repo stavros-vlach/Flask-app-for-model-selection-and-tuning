cb167e9983ad8a690a537a8a73e19311a2ecc175
Stavros Vlachoutsikos <122750141+stavros-vlach@users.noreply.github.com>
2025-09-05 22:18:50 +0300

    Update model.py

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from utils import plot_confusion_matrix
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+import joblib
 
 available_models = {
 	"logistic_regression": LogisticRegression,
@@ -16,23 +17,35 @@ available_models = {
 
 def train_model(model_name, X, y, **kwargs):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    parameters = kwargs
     
     if model_name not in available_models:
         raise ValueError(f"Unknown model: {model_name}")
         
-    if model_name in ["logistic_regression", "svm"]:
+    if model_name  == "logistic_regression":
+        model = available_models[model_name]
         pipeline = Pipeline([
             ("scaler", StandardScaler()),
-            ("model", available_models[model_name]())
+            ("model", model(C=float(parameters['C']), max_iter=int(parameters['max_iter'])))
+        ])
+    elif model_name == "svm":
+        model = available_models[model_name]
+        pipeline = Pipeline([
+            ("scaler", StandardScaler()),
+            ("model", model(C=parameters['C'], kernel=parameters['kernel']))
         ])
     else:
+        model = available_models[model_name]
         pipeline = Pipeline([
-            ("model", available_models[model_name]())
+            ("model", model(n_estimators=parameters["n_estimators"], max_depth=parameters["max_depth"]))
         ])
+    
+    print("Using model:", pipeline)
 
     pipeline.fit(X_train, y_train)
     
     y_pred = pipeline.predict(X_test)
+    
     
     accuracy = accuracy_score(y_test, y_pred)
     recall = recall_score(y_test, y_pred, average="macro")
@@ -48,5 +61,6 @@ def train_model(model_name, X, y, **kwargs):
             "recall": recall,
             "f1_score": f1,
             "accuracy": accuracy,
-            "confusion_matrix": plot_image_str
+            "confusion_matrix": plot_image_str,
+            "pipeline": pipeline
             }
